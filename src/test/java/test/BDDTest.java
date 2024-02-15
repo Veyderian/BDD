da57708aa9caef5add914961a3eed857cf6ade0a
Olga Korol <oggi.fine@gmail.com>
2024-02-15 20:54:55 +0300

    initail commit

---
+++ src/test/java/test/BDDTest.java
@@ -0,0 +1,83 @@
+package test;
+
+import com.codeborne.selenide.Selenide;
+import data.DataHelper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import page.DashboardPage;
+import page.LoginPage;
+
+import java.nio.channels.FileChannel;
+
+import static data.DataHelper.*;
+import static java.nio.channels.FileChannel.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BDDTest {
+    DashboardPage dashboardPage;
+    CardInfo firstCardInfo;
+    CardInfo secondCardInfo;
+    int firstCardBalance;
+    int secondCardBalance;
+
+@BeforeEach
+void setup() {
+    var loginPage = Selenide.open("http://localhost:9999",LoginPage.class);
+
+    //var loginPage = open("http://localhost:9999", LoginPage.class);
+//   dashboardPage.getCardBalance(1);
+        var authInfo = getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = getVerificationCode();
+        dashboardPage = verificationPage.validVerify(verificationCode);
+        firstCardInfo = getFirstCardInfo();
+        secondCardInfo = getSecondCardInfo();
+//     firstCardBalance = dashboardPage.getCardBalance(firstCardInfo);
+//    secondCardBalance = dashboardPage.getCardBalance(secondCardInfo);
+         }
+
+         @Test //1
+    void shouldTransferFromFirstToSecond() {
+         firstCardBalance = dashboardPage.getCardBalance(firstCardInfo);
+         secondCardBalance = dashboardPage.getCardBalance(secondCardInfo);
+        var amount = generateValidAmount(firstCardBalance);
+        var expectedBalanceFirstCard = firstCardBalance - amount;
+        var expectedBalanceSecondCard = secondCardBalance + amount;
+        var transferPage = dashboardPage.selectCardToTransfer(secondCardInfo);
+        dashboardPage = transferPage.makeValidTransfer(String.valueOf(amount), firstCardInfo);
+        var actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo);
+        var actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo);
+        assertEquals(expectedBalanceFirstCard, actualBalanceFirstCard);
+        assertEquals(expectedBalanceSecondCard, actualBalanceSecondCard);
+
+         }
+
+    @Test //2
+    void shouldTransferFromSecondToFirst() {
+//        var firstCardBalance = dashboardPage.getCardBalance(firstCardInfo);
+//        var secondCardBalance = dashboardPage.getCardBalance(secondCardInfo);
+        var amount = generateValidAmount(secondCardBalance);
+        var expectedBalanceSecondCard = secondCardBalance - amount;
+        var expectedBalanceFirstCard = firstCardBalance + amount;
+        var transferPage = dashboardPage.selectCardToTransfer(firstCardInfo);
+        dashboardPage = transferPage.makeValidTransfer(String.valueOf(amount), secondCardInfo);
+        var actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo);
+        var actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo);
+        assertEquals(expectedBalanceSecondCard, actualBalanceSecondCard);
+        assertEquals(expectedBalanceFirstCard, actualBalanceFirstCard);
+
+    }
+
+    @Test //3
+    void shouldGetErrorMassageIfAmountMoreBalance() {
+    var amount = generateValidAmount(secondCardBalance);
+        var transferPage = dashboardPage.selectCardToTransfer(firstCardInfo);
+       transferPage.makeTransfer(String.valueOf(amount), secondCardInfo);
+        transferPage.findErrorMassage("Выполнена попытка перевода суммы, превышающей остаток на карте списания");
+        var actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo);
+        var actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo);
+        assertEquals(firstCardBalance, actualBalanceFirstCard);
+        assertEquals(secondCardBalance, actualBalanceSecondCard);
+
+    }
+}
